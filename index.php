711a2b3618b8e2f8dc9f22dae16aa6a7ea977419
Trotteriniku <stefanozidda3@gmail.com>
2023-11-29 19:41:56 +0100

    Bonus1+Bonus2+ partials comp

--- index.php
+++ index.php
@@ -1,70 +1,54 @@
 <?php
-include 'Model/db.php';
-
-
+include __DIR__ . '/partials/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<main class="container ">
+    <div class="container-sm bg-warning rounded-3 p-0 py-4 px-4">
+        <table class="table table-striped table-dark rounded-3">
+            <thead>
+                <tr>
+                    <th scope="col">
+                        <?php ?>
+                    </th>
+                    <th scope="col">NAME</th>
+                    <th scope="col">INFO</th>
+                    <th scope="col">PARKING</th>
+                    <th scope="col">RAITING</th>
+                    <th scope="col">DISTANCE TO CENTER</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $info) { ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-
-<body style="background-color: #222222;">
-    <main class="container ">
-        <div class="container-sm bg-warning rounded-3 p-5" style="margin: 100px auto">
-            <table class="table table-striped table-dark rounded-3">
-                <thead>
                     <tr>
-                        <th scope="col">
-                            <?php ?>
-                        </th>
-                        <th scope="col">NAME</th>
-                        <th scope="col">INFO</th>
-                        <th scope="col">PARKING</th>
-                        <th scope="col">RAITING</th>
-                        <th scope="col">DISTANCE TO CENTER</th>
+                        <th scope="row"></th>
+                        <td>
+                            <?php echo ($info['name']) ?>
+                        </td>
+                        <td>
+                            <?php echo ($info['description']) ?>
+                        </td>
+                        <td>
+                            <?php echo ($info['parking'] ? '1' : '0'); ?>
+                        </td>
+                        <td>
+                            <?php echo ($info['vote']) ?>
+                        </td>
+                        <td>
+                            <?php echo ($info['distance_to_center']) ?>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($hotels as $info) { ?>
-
-                        <tr>
-                            <th scope="row"></th>
-                            <td>
-                                <?php echo ($info['name']) ?>
-                            </td>
-                            <td>
-                                <?php echo ($info['description']) ?>
-                            </td>
-                            <td>
-                                <?php echo ($info['parking'] ? '1' : '0'); ?>
-                            </td>
-                            <td>
-                                <?php echo ($info['vote']) ?>
-                            </td>
-                            <td>
-                                <?php echo ($info['distance_to_center']) ?>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                <?php } ?>
+            </tbody>
+        </table>
 
 
-        </div>
-    </main>
+    </div>
+</main>
 
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ partials/header.php
@@ -0,0 +1,45 @@
+<?php
+include __DIR__ . '/../Model/db.php';
+
+if (isset($_GET['parking']) && isset($_GET['vote'])) {
+    $category = $_GET['parking'];
+    $raiting = $_GET['vote'];
+
+
+    $hotels = array_filter($hotels, fn($info) => $info['vote'] > $raiting);
+    $hotels = array_filter($hotels, fn($info) => $category == 'all' || $category == $info['parking']);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body style="background-color: #222222;">
+
+
+    <header class="container mt-5 mb-3  ">
+        <form action="index.php" method="GET">
+            <select class="p-1" aria-label="Default select example" name="parking">
+                <option selected value="all">All</option>
+                <option value="true">With parking</option>
+                <option value="false">no parking</option>
+            </select>
+            <select class="p-1" aria-label="Default select example" name="vote">
+                <option value="0">1</option>
+                <option value="1">2</option>
+                <option value="2">3</option>
+                <option value="3">4</option>
+                <option value="4">5</option>
+            </select>
+            <button type="submit" class="btn btn-outline-warning align-baseline ">Invia</button>
+        </form>
+    </header>
